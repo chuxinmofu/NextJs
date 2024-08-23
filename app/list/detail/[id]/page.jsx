fbc8322cece9583a776b51837cf40d1c8a84d705
macong2023 <macong@jessieray.cn>
2024-08-23 09:20:50 +0800

    feat：创建动态路由

---
+++ app/list/detail/[id]/page.jsx
@@ -0,0 +1,10 @@
+
+const ListDetail = ({ params }) => {
+    
+    return (
+        <>
+            {params.id}
+        </>
+    )
+}
+export default ListDetail
--- components/List/index.jsx
+++ components/List/index.jsx
@@ -1,6 +1,11 @@
+'use client'
+import React, { useState } from 'react';
+import Link from "next/link";
+
 const ListCom = () => {
+    const [id, setId] = useState('00001')
     return (
-        <>List</>
+        <Link href={`/list/detail/${id}`}>To Detail</Link>
     )
 }
 export default ListCom
